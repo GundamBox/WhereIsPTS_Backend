bec6d1e773e1e29f371a49926138dca19eb0178d
NoobTW <noob@noob.tw>
2019-04-18 10:00:09 +0800

    fix: add CORS on api

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import logging
 import os
 
 from flask import Blueprint, Flask
+from flask_cors import CORS
 
 from app.utils import common
 
@@ -9,6 +10,7 @@ from app.utils import common
 def create_app(config_path=None):
 
     app = Flask(__name__)
+    CORS(app)
 
     if config_path is None and 'PROJECT_SETTINGS' in os.environ:
         app.config.from_object(os.environ['PROJECT_SETTINGS'])

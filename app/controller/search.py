a3bdc86455f19d606decfb71611be5749411cb15
Gundam <gundambox.shi@gmail.com>
2019-04-18 22:29:49 +0800

    edit: update status code

--- app/controller/search.py
+++ app/controller/search.py
@@ -6,10 +6,10 @@ search = Blueprint('search', __name__)
 @search.route('/api/search/<string:name>', methods=['GET'])
 def search_store(name):
     store_list = Store.search_by_name(name)
-    return jsonify([store.serialize() for store in store_list])
+    return jsonify([store.serialize() for store in store_list]), 200
 
 
 @search.route('/api/search/<float:lat>,<float:lng>', methods=['GET'])
 def search_nearby(lat, lng):
     store_list = Store.search_by_lat_lng(float(lat), float(lng), 10.0)
-    return jsonify([store.serialize() for store in store_list])
+    return jsonify([store.serialize() for store in store_list]), 200
--- app/controller/store.py
+++ app/controller/store.py
@@ -9,7 +9,10 @@ store = Blueprint('store', __name__)
 @store.route('/api/store/<int:store_id>', methods=['GET'])
 def get_store(store_id):
     store = Store.get_by_id(store_id)
-    return jsonify(store.serialize())
+    if store
+    return jsonify(store.serialize()), 200
+    else:
+        return Response(status=404)
 
 
 @store.route('/api/store', methods=['POST'])
@@ -24,9 +27,9 @@ def create_store():
                   request.remote_addr)
     success = store.create()
     if success:
-        return Response(status=201)
+        return store.id, 200
     else:
-        return Response(status=500)
+        return Response(status=400)
 
 
 @store.route('/api/store/<int:store_id>', methods=['PUT'])
@@ -46,6 +49,6 @@ def edit_store(store_id):
     success = s.update()
 
     if success:
-        return Response(status=204)
+        return Response(status=200)
     else:
-        return Response(status=500)
+        return Response(status=400)

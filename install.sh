bc18b4783d8e4a83bbbcf1f57712adb8d1382c7d
Gundam <gundambox.shi@gmail.com>
2019-05-01 20:07:22 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Python
+sudo apt-get install -y python3 python3-dev python3-pip
+
 # libpq-dev
 sudo apt-get install -y libpq-dev
+
 # PostgreSQL
 sudo apt-get install -y postgresql postgresql-contrib
 # PostGIS
 sudo add-apt-repository -y ppa:ubuntugis/ppa
 sudo apt-get update
 sudo apt-get install -y postgis
+
 # Supervisor
 sudo apt-get install -y supervisor
 # Virtualenv
@@ -22,4 +27,7 @@ source pyenv/bin/activate
 pip install -r requirements/common.txt
 
 # Make config
-cp config_example.py config.py
+sudo cp config_example.py config.py
+
+# Copy Supervisor config
+sudo cp WhereIsPTS_API.conf /etc/supervisor/conf.d/WhereIsPTS_API.conf

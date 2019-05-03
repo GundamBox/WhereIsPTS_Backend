b498772201fd3df11e142a4cfbe85d469cd99ae3
Gundam <gundambox.shi@gmail.com>
2019-05-03 20:23:07 +0800

    fix typing error

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_database WHERE datname = 'whereispts';" | grep -q 1 || sudo -u postgres -H -- psql -c  "CREATE DATABASE whereispts;"
 
 # Install production requirements
-sudo pip3 install -r reqirements.txt
+sudo pip3 install -r requirements.txt
 
 # Set enivornment 
 export FLASK_CONFIG="production"

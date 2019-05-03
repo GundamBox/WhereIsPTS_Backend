e7a5462e16680c8711ff70aa608895e863bb6cd7
Gundam <gundambox.shi@gmail.com>
2019-05-03 20:35:52 +0800

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Create production user
+sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'pts';" | grep -q 1 || sudo -u postgres -H -- psql -c  "CREATE ROLE user LOGIN PASSWORD 'please_change_password';"
+
 # Create production database if not exists
-sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_database WHERE datname = 'whereispts';" | grep -q 1 || sudo -u postgres -H -- psql -c  "CREATE DATABASE whereispts;"
+sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_database WHERE datname = 'whereispts';" | grep -q 1 || sudo -u postgres -H -- psql -c  "CREATE DATABASE whereispts OWNER user;"
 
 # Install production requirements
 sudo pip3 install -r requirements.txt
@@ -10,4 +13,4 @@ sudo pip3 install -r requirements.txt
 export FLASK_CONFIG="production"
 
 # Migrate database to newest
-python3 manage.py upgrade
+python3 manage.py db upgrade

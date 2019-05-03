fb81cc9d0b3120cb7b9b70d3477cc8f840c080ae
Gundam <gundambox.shi@gmail.com>
2019-05-03 20:21:16 +0800

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,7 @@
 sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_database WHERE datname = 'whereispts';" | grep -q 1 || sudo -u postgres -H -- psql -c  "CREATE DATABASE whereispts;"
 
 # Install production requirements
-source pyenv/bin/activate
-sudo pip install -r reqirements.txt
+sudo pip3 install -r reqirements.txt
 
 # Set enivornment 
 export FLASK_CONFIG="production"

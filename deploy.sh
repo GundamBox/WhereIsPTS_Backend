092060c74abad844937790993cf028aa77e15f56
Gundam <gundambox.shi@gmail.com>
2019-05-03 20:49:57 +0800

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,11 @@ sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_catalog.pg_roles WHERE rolname 
 # Create production database if not exists
 sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_database WHERE datname = 'whereispts';" | grep -q 1 || sudo -u postgres -H -- psql -c  "CREATE DATABASE whereispts OWNER user;"
 
+# Create extension
+sudo -u postgres -H -- psql -d whereispts -c "CREATE EXTENSION postgis;"
+
 # Install production requirements
 sudo pip3 install -r requirements.txt
 
-# Set enivornment 
-export FLASK_CONFIG="production"
-
 # Migrate database to newest
-python3 manage.py db upgrade
+env FLASK_CONFIG="production" sudo -E python3 manage.py db upgrade

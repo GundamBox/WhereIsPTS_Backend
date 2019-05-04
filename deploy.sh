7dc41c50c3f92d9b6ec0700d953091a658752b82
Gundam <gundambox.shi@gmail.com>
2019-05-04 21:04:31 +0800

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Create production user
-sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'pts';" | grep -q 1 || sudo -u postgres -H -- psql -c  "CREATE ROLE user LOGIN PASSWORD 'please_change_password';"
+sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_catalog.pg_roles WHERE rolname = 'user';" | grep -q 1 || sudo -u postgres -H -- psql -c  "CREATE ROLE user LOGIN PASSWORD 'please_change_password';"
 
 # Create production database if not exists
 sudo -u postgres -H -- psql -c "SELECT 1 FROM pg_database WHERE datname = 'whereispts';" | grep -q 1 || sudo -u postgres -H -- psql -c  "CREATE DATABASE whereispts OWNER user;"
 
 # Create extension
-sudo -u postgres -H -- psql -d whereispts -c "CREATE EXTENSION postgis;"
+sudo -u postgres -H -- psql -d whereispts -c "CREATE EXTENSION IF NOT postgis;"
 
 # Install production requirements
 sudo pip3 install -r requirements.txt

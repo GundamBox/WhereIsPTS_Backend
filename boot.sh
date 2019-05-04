088c460981b6f37650be316ab5eb279b3473ebba
Gundam <gundambox.shi@gmail.com>
2019-05-04 21:44:37 +0800

    fix fucking stupid bug

--- boot.sh
+++ boot.sh
@@ -4,4 +4,4 @@
 export FLASK_CONFIG="production"
 
 # Run server
-gunicorn "app:create_app()" -b 127.0.0.1:8000 -reload -max-requests 1
+gunicorn "app:create_app()" -b 127.0.0.1:8000 --reload --max-requests 1

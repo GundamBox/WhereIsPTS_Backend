6c796c992d1d38c36bde72ac2e668c9fb6322478
rapirent <rapirent@gmail.com>
2019-05-05 17:24:46 +0800

    update some script & surrogate function

--- config_example.py
+++ config_example.py
@@ -1,8 +1,16 @@
 import os
 
+def get_secret(setting):
+    try:
+        return os.environ[setting]
+    except KeyError:
+        error_msg = "Should set the {} enviroment variable".format(setting)
+        print(error_msg)
+        return None
+
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SECRET_KEY = get_secret('SECRET_KEY') or 'hard to guess string'
 
     @staticmethod
     def init_app(app):
@@ -11,18 +19,18 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = get_secret('DEV_DATABASE_URL') or \
         'postgresql://<user_name>:<user_password>@localhost/whereispts_dev'
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = get_secret('TEST_DATABASE_URL') or \
         'postgresql://<user_name>:<user_password>@localhost/whereispts_test'
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = get_secret('DATABASE_URL') or \
         'postgresql://<user_name>:<user_password>@localhost/whereispts'
 
     @classmethod

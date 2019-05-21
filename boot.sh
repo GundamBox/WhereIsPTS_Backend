5335cbc60c51a5fc4b0278a0ae12db12dfdaee2b
Gundam <gundambox.shi@gmail.com>
2019-05-21 21:44:00 +0800

    fix boot.sh

--- boot.sh
+++ boot.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Set enivornment 
-export FLASK_CONFIG="production"
-
 # Run server
-gunicorn "app:create_app()" -b 127.0.0.1:8000 --reload --max-requests 1
+gunicorn "app:create_app(\"production\")" -b 127.0.0.1:8000 --reload --max-requests 1
